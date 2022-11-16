95d9be3148d00185cefb18533814ea420f77f7cf
tunguyen261 <nguyendoantu2610@gmail.com>
2022-11-16 13:54:42 +0700

    dry

---
+++ src/main/java/com/fpt/dry/DryApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.dry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fpt/dry/DryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.dry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
